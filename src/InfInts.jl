f2673791c05a5642c4d942ed1260a980761c1571
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-30 00:33:29 -0400

    Update InfInts.jl

--- src/InfInts.jl
+++ src/InfInts.jl
@@ -5,19 +5,75 @@ export InfInt32, InfInt64
 import Base.Checked: add_with_overflow, sub_with_overflow, mul_with_overflow
 #checked_neg, checked_abs, checked_add, checked_sub, checked_mul,
 #                     checked_div, checked_rem, checked_fld, checked_mod, checked_cld,
- truct Special64
+struct InfInt
     value::Int64
 end
 
-const PosInf = Special(1)
-const NegInf = Special(-1)
-const InvInf = Special(0)
+const PosInf64 = typemax(Int64)
+const NegInf64 = typemin(Int64)
 
-const InfInt = Union{Int64, Special64}
+const PosInf = InfInt(PosInf64)
+const NegInf = InfInt(NegInf64)
+const ZerInf = InfInt(Int64(0))
 
-function InfInt(x::Int64)
-  iszero(x) ? InvInf : x
-end  
+Base.isinf(x::InfInt) = x.value === PosInf64 || x.value === NegInf64
+Base.iszero(x::InfInt) = iszero(x.value)
+Base.isone(x::InfInt) = iszero(x.value)
 
+function Base.:(*)(x::InfInt, y::InfInt)
+    res, ovf = mul_with_overflow(x.value, y.value)
+    if ovf
+        signbit(x.value) === signbit(y.value) ? PosInf : NegInf
+    else
+        InfInt(res)
+    end
+end
+
+function Base.:(*)(x::InfInt, y::Int64)
+    res, ovf = mul_with_overflow(x.value, y)
+    if ovf
+        signbit(x.value) === signbit(y) ? PosInf : NegInf
+    else
+        InfInt(res)
+    end
+end
+
+function Base.:(*)(x::Int64, y::InfInt)
+    res, ovf = mul_with_overflow(x, y.value)
+    if ovf
+        signbit(x) === signbit(y.value) ? PosInf : NegInf
+    else
+        InfInt(res)
+    end
+end
+
+function Base.:(+)(x::InfInt, y::InfInt)
+    res, ovf = add_with_overflow(x.value, y.value)
+    if ovf # both signbits match
+        signbit(x.value) ? NegInf : PosInf
+    else
+        InfInt(res)
+    end
+end
+
+function Base.:(-)(x::InfInt, y::InfInt)
+    res, ovf = sub_with_overflow(x.value, y.value)
+    if ovf # signbits do not match
+        signbit(res) ? PosInf : NegInf
+    else
+        InfInt(res)
+    end
+end
+
+function Base.div(x::InfInt, y::InfInt)
+    x === 0 && y === 0 && throw(DivideError,"integer division error: div(0,0)")
+    isinf(x) && isinf(y) && throw(DivideError,"integer division error: div(Inf,Inf)")
+    iszero(x) && return ZerInf
+    if isinf(x)
+        signbit(x.value) === signbit(y.value) ? x : -x
+    else
+        InfInt(div(x.value, y.value))
+    end
+end
 
 end  # InfInts
