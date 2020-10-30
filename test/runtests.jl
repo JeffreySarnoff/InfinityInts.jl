e5218faafd151a29ed7af0526079552417f7454e
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2020-10-30 03:44:22 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,42 @@
 using InfinityInts, Test
 
-@test true
+a = InfInt(10)
+b = InfInt(5)
+c = InfInt(50)
+d = InfInt(2)
+e = InfInt(1)
+f = InfInt(0)
+
+@test a == a
+@test a === a
+@test a != b
+@test a !== b
+
+@test a <= b
+@test a < b
+@test d >= e
+@test d > e
+
+@test b + b == a
+@test a - b === b
+@test a * b === c
+
+@test div(c, b) == a
+@test div(c, -b) == -a
+@test div(-c, b) == -a
+@test div(-c, -b) == a
+
+@test rem(b, d) == e
+@test rem(-b, d) == -e
+@test rem(b, -d) == e
+@test rem(-b, -d) == -e
+
+@test fld(c, b) == a
+@test fld(-c, b) == -a
+@test fld(c, -b) == -a
+@test fld(-c, -b) == a
+
+@test mod(b, d) == e
+@test mod(-b, d) == e
+@test mod(b, -d) == -e
+@test mod(-b, -d) == -e
